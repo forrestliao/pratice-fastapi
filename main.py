be40e24dc9b655fb07bc1dcef4495f65c03e5841
forrest <forrest_liao@wistron.com>
2020-07-22 19:57:40 +0800

    fisrt program

---
+++ main.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from fastapi import applications
+from fastapi.openapi.docs import get_swagger_ui_html
+def swagger_monkey_patch(*args, **kwargs):
+    """
+    Wrap the function which is generating the HTML for the /docs endpoint and 
+    overwrite the default values for the swagger js and css.
+    """
+    return get_swagger_ui_html(
+        *args, **kwargs,
+        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.29/swagger-ui-bundle.js",
+        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.29/swagger-ui.css")
+applications.get_swagger_ui_html = swagger_monkey_patch
+
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str]= None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_price": item.price, "item_id": item_id}
