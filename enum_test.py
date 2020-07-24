2748b630d954c8f5976fd1354befd1a1fcfdaf41
forrest_liao <z9508010531@gmail.com>
2020-07-24 18:17:30 +0800

    new enum

---
+++ enum_test.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+app = FastAPI()
+
--- main.py
+++ main.py
@@ -25,13 +25,18 @@ class Item(BaseModel):
     is_offer: Optional[bool] = None
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Hello World"}
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str]= None):
+#     return {"item_id": item_id, "q": q}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str]= None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    return {"item_id": item_id}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_price": item.price, "item_id": item_id}
+
